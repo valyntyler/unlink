18aa18f62fddbd4f67729b65d7a8d743d20f62ee
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-07 14:54:19 +0200

    feat: packaged for nix

--- flake.nix
+++ flake.nix
@@ -13,12 +13,26 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        unlink = pkgs.stdenv.mkDerivation rec {
+          name = "unlink";
+          src = ./.;
+          buildInputs = with pkgs; [nushell];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ./main.nu $out/bin/${name}
+            chmod +x $out/bin/${name}
+          '';
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nushell
+            unlink
           ];
         };
+
+        packages.default = unlink;
+        packages.unlink = unlink;
       }
     );
 }
